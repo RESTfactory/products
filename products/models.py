ec41ab53a41a3eb75efac3f777d786a9ad8258d9
mcueto <cueto@live.cl>
2016-10-20 16:07:43 -0300

    Added new models

--- products/models.py
+++ products/models.py
@@ -25,3 +25,45 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+#Provisional model
+class Client(models.Model):
+    name = models.CharField(max_length=30)
+    code = models.CharField(max_length=40, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductInstance(models.Model):
+    name = models.CharField(max_length=30)
+    category = models.ForeignKey(Category, blank=True)
+    product = models.ForeignKey(Product, blank=True)
+    client = models.ForeignKey(Client, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+#Provisional model
+class Local(models.Model):
+    name = models.CharField(max_length=30)
+    code = models.CharField(max_length=40, unique=True)
+    place_id = models.CharField(max_length=40, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductData(models.Model):
+    product_instance = models.ForeignKey(ProductInstance)
+    local = models.ForeignKey(Local)
+
+    price = models.FloatField(blank=True, null=True)
+    share = models.IntegerField(blank=True, null=True)
+    # status = disponible, ajuste, maestra, abastecimiento
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.created_at
