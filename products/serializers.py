a1b36e8336c2cb9a203ee1df56f1b52fc152c256
mcueto <cueto@live.cl>
2016-10-25 18:35:14 -0300

    Disabled jsonapi
    The deadline dont let us rest!

--- products/serializers.py
+++ products/serializers.py
@@ -1,38 +1,48 @@
 from rest_framework import serializers
+from rest_framework_json_api.relations import ResourceRelatedField
 from .models import Brand, Category, Product, Client, ProductInstance, Local, Provision, ProductStatus, ProductData
 
 class BrandSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Brand
+        fields = ["url", "id", "name"]
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
+        fields = [ "url", "id", "name", "code", "description"]
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
+        fields = [ "url", "id", "sku", "name", "brand", "category"]
 
 class ClientSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Client
+        fields = [ "url", "id", "name", "code"]
 
 class ProductInstanceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ProductInstance
+        fields = [ "url", "id", "name", "category", "product", "client"]
 
 class LocalSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Local
+        fields = [ "url", "id", "name", "code", "place_id"]
 
 class ProvisionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Provision
+        fields = ["url", "id", "start_at", "end_at", "local", "product_instances"]
 
 class ProductStatusSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ProductStatus
+        fields = ["url", "id", "code", "name", "client"]
 
 class ProductDataSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ProductData
+        fields = ["url", "id", "price", "shelf_share", "shelf_stock", "product_instance", "local", "provision", "status"]
