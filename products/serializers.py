7c406b89e3c04b691bf61f90ebd4524525eb03fb
mcueto <cueto@live.cl>
2016-10-26 18:30:49 -0300

    Added relationships in serializers

--- products/serializers.py
+++ products/serializers.py
@@ -13,6 +13,9 @@ class CategorySerializer(serializers.HyperlinkedModelSerializer):
         fields = [ "url", "id", "name", "code", "description"]
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    brand = BrandSerializer(many=False, read_only=False)
+    category = CategorySerializer(many=False, read_only=False)
+
     class Meta:
         model = Product
         fields = [ "url", "id", "sku", "name", "brand", "category"]
@@ -23,6 +26,10 @@ class ClientSerializer(serializers.HyperlinkedModelSerializer):
         fields = [ "url", "id", "name", "code"]
 
 class ProductInstanceSerializer(serializers.HyperlinkedModelSerializer):
+    category = CategorySerializer(many=False, read_only=False)
+    product = ProductSerializer(many=False, read_only=False)
+    client = ClientSerializer(many=False, read_only=False)
+
     class Meta:
         model = ProductInstance
         fields = [ "url", "id", "name", "category", "product", "client"]
@@ -33,16 +40,26 @@ class LocalSerializer(serializers.HyperlinkedModelSerializer):
         fields = [ "url", "id", "name", "code", "place_id"]
 
 class ProvisionSerializer(serializers.HyperlinkedModelSerializer):
+    local = LocalSerializer(many=False, read_only=False)
+    product_instances = ProductInstanceSerializer(many=True, read_only=False)
+
     class Meta:
         model = Provision
         fields = ["url", "id", "start_at", "end_at", "local", "product_instances"]
 
 class ProductStatusSerializer(serializers.HyperlinkedModelSerializer):
+    client = ClientSerializer(many=False, read_only=False)
+
     class Meta:
         model = ProductStatus
         fields = ["url", "id", "code", "name", "client"]
 
 class ProductDataSerializer(serializers.HyperlinkedModelSerializer):
+    product_instance = ProductInstanceSerializer(many=False, read_only=False)
+    local = LocalSerializer(many=False, read_only=False)
+    provision = ProvisionSerializer(many=False, read_only=False)
+    status = ProductStatusSerializer(many=False, read_only=False)
+
     class Meta:
         model = ProductData
         fields = ["url", "id", "price", "shelf_share", "shelf_stock", "product_instance", "local", "provision", "status"]
